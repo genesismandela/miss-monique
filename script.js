d13e849dc28ea65d5989504fd411ba6dd4bd2b7d
genesismandela <mandelasamboy@gmail.com>
2021-01-18 13:17:50 +0000

    Testing website

--- script.js
+++ script.js
@@ -35,17 +35,31 @@ const loader = document.querySelector(".loader");
 window.addEventListener("load", function () {
 	setTimeout(function () {
 		loader.style.display = "none";
-	}, 1000);
+	}, 5000);
 });
 
 // ///////////////
 // // HEADER NAVIGATION BURGER
 const openNavMenu = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".header__navigation__list");
+const navLink = document.querySelectorAll(".header__navigation__link");
+
+const windowWidth = window.innerWidth;
+
+const NavMenuPhones = function () {
+	if (windowWidth <= 768) {
+		navLink.forEach(function (item, idk) {
+			item.addEventListener("click", function () {
+				navMenu.style.visibility = "hidden";
+				openNavMenu.classList.toggle("is-active");
+				console.log("btn clicked");
+			});
+		});
+	}
+};
+NavMenuPhones();
 
 openNavMenu.addEventListener("click", function () {
-	// navMenu.style.visibility = "visible";
-
 	if (navMenu.style.visibility === "visible") {
 		navMenu.style.visibility = "hidden";
 		openNavMenu.classList.toggle("is-active");
@@ -53,6 +67,7 @@ openNavMenu.addEventListener("click", function () {
 	} else {
 		navMenu.style.visibility = "visible";
 		openNavMenu.classList.toggle("is-active");
+		console.log("opened");
 	}
 });
 
@@ -83,3 +98,60 @@ btnShowHideSongs.addEventListener("click", function () {
 		btnShowHideSongs.textContent = "Show less";
 	}
 });
+
+const videoSlider = document.querySelector(".mimo-podcast__youtube__slider"); // Variable with the video slider
+const videoSlides = document.querySelectorAll(
+	".mimo-podcast__youtube__slider__video",
+); // Variable with the video slides
+
+// GO BACK & NEXT ARROWS
+const goBack = document.querySelector(".mimo-podcast__youtube__nav__btn--back");
+const goNext = document.querySelector(".mimo-podcast__youtube__nav__btn--next");
+
+const mimoTitle = document.querySelector(".mimo-podcast__youtube__title");
+
+let currentSlide = 0;
+const maxSlide = videoSlides.length;
+
+const goToSlide = function (slide) {
+	videoSlides.forEach(
+		(s, i) => (s.style.transform = `translateX(${150 * (i - slide)}%`),
+	);
+};
+goToSlide(0);
+
+// Function to make the Right Arrow work to slide between videos
+const nextSlide = function () {
+	if (currentSlide === maxSlide - 1) {
+		currentSlide = 0;
+	} else {
+		currentSlide++;
+	}
+	goToSlide(currentSlide);
+	swapTitle();
+};
+
+// Function to make the Left Arrow work to slide between videos
+const prevSlide = function () {
+	if (currentSlide === 0) {
+		currentSlide = maxSlide - 1;
+	} else {
+		currentSlide--;
+	}
+	goToSlide(currentSlide);
+	swapTitle();
+};
+
+const swapTitle = function () {
+	if (currentSlide === 0) {
+		mimoTitle.textContent = "Miss Monique - MiMo 024";
+	} else if (currentSlide === 1) {
+		mimoTitle.textContent = "Miss Monique - MiMo 023";
+	} else if (currentSlide === 2) {
+		mimoTitle.textContent = "Miss Monique - MiMo 022";
+	}
+	goToSlide(currentSlide);
+};
+
+goNext.addEventListener("click", nextSlide);
+goBack.addEventListener("click", prevSlide);
